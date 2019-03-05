a45323729784cee83557dc2dfa5445241465454d
Saket Juneja <juneja.saket@gmail.com>
2019-03-04 21:48:37 -0500

    Add project files.

---
+++ ML.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ML.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
